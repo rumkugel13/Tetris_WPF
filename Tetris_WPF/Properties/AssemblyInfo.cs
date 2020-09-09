d35d51d05904f738040d43de479852a5c8f2ecb0
rumkugel13 <rumkugel13@outlook.com>
2020-09-09 21:28:25 +0200

    update copyright info

--- Tetris_WPF/Properties/AssemblyInfo.cs
+++ Tetris_WPF/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tetris_WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © rumkugel13 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
